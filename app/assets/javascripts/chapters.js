1710f251eba02252dc4bec46b109f6a3f8882e4b
Murtaza Ali <murtaza7@gmail.com>
2012-11-25 15:48:43 -0800

    goes to the next chapter when you reach the last page and shows different text if chapter is last in series

--- app/assets/javascripts/chapters.js
+++ app/assets/javascripts/chapters.js
@@ -20,15 +20,42 @@ $(document).ready(function(){
 			}
 		});
 	};
+
+// Navigating to another chapter	
+
+	var chapters = $('#chapter-dropdown a');
+
+	$('#last_chapter').hide();
+	
+	var next_chapter = function(){
+		var current_chapter_title = $('#current_chapter_title').text();
+		var current_chapter_dropdown = $('#chapters-dropdown').find("a[data-name='" + current_chapter_title + "']");
+		var current_chapter_index = $.inArray(current_chapter_dropdown[0], chapters);
+		if (chapters[current_chapter_index + 1] == undefined){
+			var active = $('[data-active="true"]');
+			hide_page(active);
+			$('#last_chapter').show(); 
+		}
+		else {	
+			var next_chapter_url = chapters[current_chapter_index + 1].href;
+			window.location.href = next_chapter_url			
+		}
+
+	}
+	////////////////////////////////
 	
 	hide_all(pages);
 
 	var	show_next = function() {
 		var active = $('[data-active="true"]');
 		var next_page = $('[data-active="true"]').next();
-		
-		hide_page(active);
-		show_page(next_page);
+		if (next_page.length < 1 ) {
+			next_chapter();
+		}
+		else {
+			hide_page(active);
+			show_page(next_page);
+		}
 	};
 
 	var show_previous = function () {
@@ -66,7 +93,6 @@ $(document).ready(function(){
 		show_page(nextPage);
 	});
 
-// Navigating to another chapter	
 	
 
 
